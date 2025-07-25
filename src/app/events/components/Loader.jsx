d3ee5a361b105aa7c6b352cd8fd4d15c63203449
Youssef97dev <youssefmoussafi97@gmail.com>
2025-07-25 16:46:31 +0100

    change text 9

--- src/app/events/components/Loader.jsx
+++ src/app/events/components/Loader.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Hero from "./Hero";
@@ -9,6 +9,10 @@ import Experts from "./Experts";
 
 const Loader = () => {
   const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <>
