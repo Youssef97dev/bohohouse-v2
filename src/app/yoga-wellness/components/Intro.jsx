37f4d7964219170520f338729835325b7d6b59b9
Youssef97dev <youssefmoussafi97@gmail.com>
2025-08-28 11:17:51 +0100

    add centent yoga page 2

--- src/app/yoga-wellness/components/Intro.jsx
+++ src/app/yoga-wellness/components/Intro.jsx
@@ -6,14 +6,14 @@ import { useTranslation } from "react-i18next";
 const Intro = () => {
   const { t } = useTranslation();
   return (
-    <div className="w-full flex flex-col justify-center items-center  py-36 tracking-widest px-8 lg:px-[200px] lg:text-[16px] text-[16px] text-center gap-8">
+    <div className="w-full flex flex-col justify-center items-center  py-36 tracking-widest px-8 lg:px-[200px] lg:text-[16px] text-[16px] lg:text-center gap-8 text-justify">
       <p className="font-playfair text-[18px]">{t("yoga.desc_1")}</p>
       <p className="font-playfair text-[14px]">{t("yoga.desc_2")}</p>
       <p className="font-playfair text-[14px]">{t("yoga.desc_3")}</p>
       <p className="font-playfair text-[14px]">{t("yoga.desc_4")}</p>
       <Link
         href="#"
-        className={`text-center border border-black bg-bgButton text-black py-1 lg:py-2 px-5 uppercase text-[15px]`}
+        className={`text-center border border-black bg-bgButton text-black py-3 lg:py-2 px-5 uppercase text-[15px]`}
       >
         {t("yoga.button")}
       </Link>
